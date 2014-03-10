dc61d57742928464dee41681f185ced52ea60cca
xzjs <xzjs@ap.local>
2014-03-10 13:20:30 +0800

    写好了基础界面，并实现了一些简单的功能

--- PullEye/PullEye/HelloWorldViewController.m
+++ PullEye/PullEye/HelloWorldViewController.m
@@ -8,15 +8,32 @@
 
 #import "HelloWorldViewController.h"
 
-@interface HelloWorldViewController ()
+@interface HelloWorldViewController (){
+    int currentValue;
+    int targetValue;
+}
+- (IBAction)sliderMoved:(UISlider *)sender;
+- (IBAction)showAlert:(id)sender;
+@property (strong, nonatomic) IBOutlet UISlider *slider;
 
 @end
 
 @implementation HelloWorldViewController
 
+@synthesize slider;
+
+-(void)startNewRound{
+    targetValue=1+(arc4random()%100);
+    currentValue=50;
+    self.slider.value=currentValue;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+//    currentValue=self.slider.value;
+//    targetValue=1+(arc4random()%100);
+    [self startNewRound];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +43,15 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sliderMoved:(UISlider *)sender {
+    //UISlider *slider=(UISlider *)sender;
+    //NSLog(@"滑动条当前的数值是:%f",slider.value);
+    currentValue=(int)lround(sender.value);
+}
+
+- (IBAction)showAlert:(id)sender {
+    NSString *message=[NSString stringWithFormat:@"滑动条当前的数值是：%d,我们的目标数值是:%d",currentValue,targetValue];
+    [[[UIAlertView alloc] initWithTitle:@"您好，苍老师" message:message delegate:nil cancelButtonTitle:@"东京热" otherButtonTitles:nil, nil]show];
+    [self startNewRound];
+}
 @end
